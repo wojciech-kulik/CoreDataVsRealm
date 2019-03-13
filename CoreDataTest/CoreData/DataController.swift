e3c808efa66bc77a256df5a2e727d605b57bee20
Wojciech Kulik <wkulik91@gmail.com>
2019-03-13 01:15:52 +0100

    Implemented sample UI to test CoreData

--- CoreDataTest/CoreData/DataController.swift
+++ CoreDataTest/CoreData/DataController.swift
@@ -8,7 +8,7 @@ class DataController {
         return self.persistentContainer.viewContext
     }
     
-    func initalizeStack() {
+    func initalizeStack(completion: @escaping () -> Void) {
         //self.setStore(type: NSInMemoryStoreType)
         self.persistentContainer.loadPersistentStores { description, error in
             if let error = error {
@@ -16,17 +16,7 @@ class DataController {
                 return
             }
             print("store loaded")
-            
-            do {
-                try self.insertUser(withBook: true)
-                let users = try self.fetchUsers()
-                users.forEach {
-                    print([$0.firstName, $0.lastName, "\($0.bonusPoints)",
-                        $0.books.first?.title ?? ""].joined(separator: ", "))
-                }
-            } catch (let error) {
-                print("failed \(error.localizedDescription)")
-            }
+            completion()
         }
     }
     
@@ -55,13 +45,8 @@ class DataController {
         return users
     }
     
-    func insertUser(withBook: Bool) throws {
-        let user = User(context: self.context)
-        user.firstName = "John"
-        user.lastName = "Snow"
-        user.username = "john_snow12"
-        user.bonusPoints = "10"
-        
+    func insert(user: User, withBook: Bool) throws {
+
         if withBook {
             let book = Book(context: self.context)
             book.title = "A Song of Ice and Fire"
--- CoreDataTest/ViewController.swift
+++ CoreDataTest/ViewController.swift
@@ -3,10 +3,84 @@ import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     let dataController = DataController()
+    var users: [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dataController.initalizeStack()
+        self.tableView.tableFooterView = UIView()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.dataController.initalizeStack { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.users = (try? strongSelf.dataController.fetchUsers()) ?? []
+            strongSelf.tableView.reloadData()
+        }
+    }
+    
+    @IBAction func addClicked(_ sender: Any) {
+        let alertController = UIAlertController(title: "New User", message: "", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+            let user = User(context: self.dataController.context)
+            user.firstName = alertController.textFields?[0].text?.trimmingCharacters(in: CharacterSet.whitespaces) ?? ""
+            user.lastName = alertController.textFields?[1].text?.trimmingCharacters(in: CharacterSet.whitespaces) ?? ""
+            user.username = alertController.textFields?[2].text?.trimmingCharacters(in: CharacterSet.whitespaces) ?? ""
+            user.bonusPoints = "10"
+            
+            try? self.dataController.insert(user: user, withBook: true)
+            self.users = (try? self.dataController.fetchUsers()) ?? []
+            self.tableView.reloadData()
+        })
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "First name"
+        }
+        alertController.addTextField { textField in
+            textField.placeholder = "Last name"
+        }
+        alertController.addTextField { textField in
+            textField.placeholder = "Username"
+        }
+        
+        self.navigationController?.present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let user = self.users[indexPath.row]
+        
+        cell.selectionStyle = .none
+        cell.textLabel?.text = "\(user.firstName) \(user.lastName)"
+        cell.detailTextLabel?.text = user.username
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            let user = self.users[indexPath.row]
+            
+            try? self.dataController.delete(user: user)
+            self.users.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
